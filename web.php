b41174191384777f866d8dad2f405351e9ff26f9
Atif Ali Khan <35859555+aatifbangash@users.noreply.github.com>
2024-01-10 14:38:39 +0500

    Update web.php

--- web.php
+++ web.php
@@ -7,44 +7,36 @@ use Illuminate\Support\Str;
 
 Route::match(['get', 'post'], 'orm', function (Request $request) {
 
-    $correctPasscode = 'XXXXXXX'; //Set your passcode here
+    $correctPasscode = 'admin';
     $data = [];
-
-    if ($request->method() == 'POST' && $request->mode == 'logout') {
-        session(['passcode_verified' => false]);
-    }
-
-    if ($request->method() == 'POST' && $request->mode == 'passcode') {
-        if (!session('passcode_verified') && $request->passcode === $correctPasscode)
+    if ($request->isMethod('post')) {
+        if ($request->mode == 'logout') {
+            session(['passcode_verified' => false]);
+        } elseif ($request->mode == 'passcode' && !session('passcode_verified') && $request->passcode === $correctPasscode) {
             session(['passcode_verified' => true]);
-    }
-
-    if ($request->method() == 'POST' && $request->mode == 'editor') {
-        try {
-
-            $request->orm_query = Str::replace(';', '', $request->orm_query);
-            if (Str::startsWith($request->orm_query, "DB::")) {
-                $code = "return $request->orm_query;";
-                $data = eval($code);
-            } elseif (Str::contains($request->orm_query, "toSql")) {
-                $query = Str::replace('toSql', 'get', $request->orm_query);
-                $code = "return App\Models\\$query;";
-                Illuminate\Support\Facades\DB::connection()->enableQueryLog();
-                eval($code);
-//                $data = collect(DB::getQueryLog())->pluck('query')->toArray();
-//                $bindings = collect(DB::getQueryLog())->pluck('bindings')->toArray();
-                $data = Illuminate\Support\Facades\DB::getQueryLog();
-                Illuminate\Support\Facades\DB::disableQueryLog();
-            } else {
-                $code = "return App\Models\\$request->orm_query->toArray();";
-                $data = eval($code);
+        } elseif ($request->mode == 'editor' && !empty($request->orm_query) && session('passcode_verified')) {
+            try {
+                $request->orm_query = Str::replace(';', '', $request->orm_query);
+                session(['query' => $request->orm_query]);
+                if (Str::startsWith($request->orm_query, "DB::")) {
+                    $code = "return $request->orm_query;";
+                    $data = eval($code);
+                } elseif (Str::contains($request->orm_query, "toSql")) {
+                    $query = Str::replace('toSql', 'get', $request->orm_query);
+                    $code = "return App\Models\\$query;";
+                    DB::connection()->enableQueryLog();
+                    eval($code);
+                    $data = DB::getQueryLog();
+                    DB::disableQueryLog();
+                } else {
+                    $code = 'return App\Models\\' . $request->orm_query . ';';
+                    $data = eval($code);
+                }
+            } catch (\Exception $e) {
+                $data = ['message' => nl2br(addslashes(htmlspecialchars($e->getMessage())))];
             }
-
-        } catch (\Exception $e) {
-            $data = ['message' => nl2br(addslashes(htmlspecialchars($e->getMessage())))];
         }
     }
-
     return view('orm', [
         'data' => $data,
         'orm_query' => $request->orm_query ?: ''
