2086bc886e31fcb39e2c4902bf96d42482d012a2
Atif Ali Khan <35859555+aatifbangash@users.noreply.github.com>
2024-01-10 11:34:13 +0500

    Update web.php

--- web.php
+++ web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 Route::match(['get', 'post'], 'orm', function (Request $request) {
     $data = [];
@@ -9,13 +10,23 @@ Route::match(['get', 'post'], 'orm', function (Request $request) {
     if ($request->method() == 'POST') {
         try {
 
-            if(Str::startsWith($request->orm_query, "DB::")){
+            if (Str::startsWith($request->orm_query, "DB::")) {
                 $code = "return $request->orm_query;";
+                $data = eval($code);
+            } elseif (Str::contains($request->orm_query, "toSql")) {
+                $query = Str::replace('toSql', 'get', $request->orm_query);
+                $code = "return App\Models\\$query;";
+                Illuminate\Support\Facades\DB::connection()->enableQueryLog();
+                eval($code);
+//                $data = collect(DB::getQueryLog())->pluck('query')->toArray();
+//                $bindings = collect(DB::getQueryLog())->pluck('bindings')->toArray();
+                $data = Illuminate\Support\Facades\DB::getQueryLog();
+                Illuminate\Support\Facades\DB::disableQueryLog();
             } else {
                 $code = "return App\Models\\$request->orm_query->toArray();";
+                $data = eval($code);
             }
 
-            $data = eval($code);
         } catch (\Exception $e) {
             $data = ['message' => nl2br(addslashes(htmlspecialchars($e->getMessage())))];
         }
