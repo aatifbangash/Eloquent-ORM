cbbc39e0bcb32dabce1ecae6cab419e9f93340e6
Atif Ali Khan <35859555+aatifbangash@users.noreply.github.com>
2024-01-11 12:12:27 +0500

    Update web.php

--- web.php
+++ web.php
@@ -2,17 +2,37 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
 Route::match(['get', 'post'], 'orm', function (Request $request) {
+    function getModels($path)
+    {
+        $out = [];
+        $results = scandir($path);
+        foreach ($results as $result) {
+            if ($result === '.' or $result === '..') continue;
+            $filename = $path . '/' . $result;
+            if (is_dir($filename)) {
+                $out = array_merge($out, getModels($filename));
+            } else {
+                if (pathinfo($result, PATHINFO_EXTENSION) != 'php') continue;
+                $out[] = substr($result, 0, -4);
+            }
+        }
+        return $out;
+    }
+
+    $models = getModels(app_path() . "/Models");
+    sort($models);
 
     $correctPasscode = 'admin';
     $data = [];
     if ($request->isMethod('post')) {
         //I have ignored the DML operation here
         if (Str::contains($request->orm_query, ['create', 'delete', 'update', 'insert'])) {
-            return view('orm', ['data' => []]);
+            return view('orm', ['data' => [], 'models' => $models]);
         }
         if ($request->mode == 'logout') {
             session(['passcode_verified' => false]);
@@ -40,8 +60,31 @@ Route::match(['get', 'post'], 'orm', function (Request $request) {
             }
         }
     }
+
+    if ($request->isMethod('get')) {
+        if (!empty($request->model) && session('passcode_verified')) {
+            //I have ignored the DML operation here
+            if (Str::contains($request->model, ['create', 'delete', 'update', 'insert'])) {
+                return view('orm', ['data' => [], 'models' => $models]);
+            }
+            try {
+                    if(!empty($request->model)){
+                        $request->model = $request->model . '::query()
+->take(25)
+->get();';
+
+                        session(['query' => $request->model]);
+                        $code = 'return App\Models\\' . $request->model . ';';
+                        $data = eval($code);
+                    }
+            } catch (\Exception $e) {
+                $data = ['message' => nl2br(addslashes(htmlspecialchars($e->getMessage())))];
+            }
+        }
+    }
+
     return view('orm', [
         'data' => $data,
-        'orm_query' => $request->orm_query ?: ''
+        'models' => $models,
     ]);
 })->name('orm');
